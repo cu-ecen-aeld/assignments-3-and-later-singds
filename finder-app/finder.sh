4ab15c674ea224788d09c947a2657c44af39df27
singds <giacomodalsasso@outlook.com>
2023-02-17 09:31:37 +0100

    assignmnet 3 part 2 done

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $# -ne 2 ]; then
     echo "error: Two parameters must be provided"
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,7 +11,12 @@ KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+# CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE=/home/giaco/buildroot-course/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+MK="make ARCH=$ARCH CROSS_COMPILE=${CROSS_COMPILE}"
+
+# To successfully run the script you have to install the following packages:
+sudo apt install flex bison libssl-dev
 
 if [ $# -lt 1 ]
 then
@@ -35,9 +40,17 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "Start kernel build with make cmd:" $MK
+    $MK mrproper
+    # $MK distclean
+    $MK defconfig
+    $MK -j4 all
+    # $MK modules
+    $MK dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp $OUTDIR/linux-stable/arch/arm64/boot/Image $OUTDIR/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +61,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +74,51 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    $MK distclean
+    $MK defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+$MK
+$MK CONFIG_PREFIX=$OUTDIR/rootfs install
+cd $OUTDIR/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+TOOLCHAIN_PATH=${CROSS_COMPILE%/*/*}
+cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ./lib64
+cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ./lib64
+cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ./lib64
+cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ./lib
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cd ${FINDER_APP_DIR}
+cp finder-test.sh finder.sh writer $OUTDIR/rootfs/home
+mkdir -p $OUTDIR/rootfs/home/conf
+cp conf/* $OUTDIR/rootfs/home/conf
+cp autorun-qemu.sh $OUTDIR/rootfs/home
 
 # TODO: Chown the root directory
+cd $OUTDIR/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd $OUTDIR/rootfs
+find . | cpio -H newc -ov --owner root:root > $OUTDIR/initramfs.cpio
+cd $OUTDIR
+gzip -f initramfs.cpio
